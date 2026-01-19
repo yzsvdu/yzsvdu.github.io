0bc53a588b1b9576eb27967069170baf368f16fe
Vincent Duong <vnnsnnt@Vincents-MacBook-Pro.local>
2026-01-19 15:00:48 -0800

    updates:

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Vincent Duong';
+export const SITE_TITLE = 'vnnsnnt';
 export const SITE_DESCRIPTION = "Vincent's Portfolio & Blog";
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
